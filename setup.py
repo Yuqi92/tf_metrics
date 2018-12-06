56e915507acb136b6f619b245b139a41baafa191
Yuqi92 <ys2887@columbia.edu>
2018-12-06 15:01:25 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ def has_gpu():
         return False
 
 
-with open("README.md", "r") as fh:
-    long_description = fh.read(encoding='utf')
+with open("README.md", "r",encoding='utf') as fh:
+    long_description = fh.read()
 
 
 install_requires = ["numpy"]
